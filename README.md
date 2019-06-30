c413f47f6d296bc05a60e22022848158d4bd7245
nextGood <783774089@qq.com>
2019-07-01 07:52:12 +0800

    0701

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # network-action
+action 包对应 Java开发实战1200例
+net 包对应 Java网络编程
---
+++ src/main/java/net/OReillyByName.java
@@ -0,0 +1,31 @@
+package net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 显示域名对应地址
+ *
+ * @author nextGood
+ * @date 2019/7/1
+ */
+public class OReillyByName {
+    public static void main(String[] args) {
+        try {
+            InetAddress address1 = InetAddress.getByName("www.oreilly.com");
+            System.out.println(address1);
+            InetAddress address2 = InetAddress.getByName("104.118.140.196");
+            System.out.println(address2);
+            InetAddress[] addresses = InetAddress.getAllByName("www.baidu.com");
+            for (InetAddress address : addresses) {
+                System.out.println(address);
+            }
+            InetAddress localHost = InetAddress.getLocalHost();
+            System.out.println(localHost);
+            byte[] ipAddresses = new byte[]{(byte) 163, (byte) 177, (byte) 151, 109};
+            System.out.println(InetAddress.getByAddress("www.baidu.com", ipAddresses));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
