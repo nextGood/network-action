0ffe779b0e5fc4c607ac4ec75759ff5a6557d3c7
nextGood <783774089@qq.com>
2019-07-02 22:01:21 +0800

    0702

--- src/main/java/net/OReillyByName.java
+++ src/main/java/net/OReillyByName.java
@@ -16,6 +16,7 @@ public class OReillyByName {
             System.out.println(address1);
             InetAddress address2 = InetAddress.getByName("104.118.140.196");
             System.out.println(address2);
+            System.out.println(InetAddress.getByName("163.177.151.110").getCanonicalHostName());
             InetAddress[] addresses = InetAddress.getAllByName("www.baidu.com");
             for (InetAddress address : addresses) {
                 System.out.println(address);
@@ -24,6 +25,11 @@ public class OReillyByName {
             System.out.println(localHost);
             byte[] ipAddresses = new byte[]{(byte) 163, (byte) 177, (byte) 151, 109};
             System.out.println(InetAddress.getByAddress("www.baidu.com", ipAddresses));
+            System.out.println(InetAddress.getLocalHost().getHostName());
+
+
+            SecurityManager sm = new SecurityManager();
+            sm.checkConnect("www.baidu.com", 8080);
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
