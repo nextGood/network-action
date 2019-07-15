3b829b64667951ce51c86ef9ce631270ed787409
nextGood <783774089@qq.com>
2019-07-16 06:54:41 +0800

    0716

---
+++ src/main/java/net/InternetContent.java
@@ -0,0 +1,36 @@
+package net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * 解析网页中的内容
+ *
+ * @author nextGood
+ * @date 2019/7/16
+ */
+public class InternetContent {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://www.baidu.com");
+            URLConnection urlConnection = url.openConnection();
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+            inputStream.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
