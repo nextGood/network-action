70c0140eea3af6cdca5471d6b1a8a712c1a70438
nextGood <783774089@qq.com>
2019-07-19 07:40:35 +0800

    0719

---
+++ src/main/java/net/BreakPointSupervene.java
@@ -0,0 +1,13 @@
+package net;
+
+/**
+ * 网络资源断点续传
+ *
+ * @author nextGood
+ * @date 2019/7/19
+ */
+public class BreakPointSupervene {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/net/SingleThreadDownload.java
@@ -0,0 +1,41 @@
+package net;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * 网络资源的单线程下载
+ *
+ * @author nextGood
+ * @date 2019/7/18
+ */
+public class SingleThreadDownload {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("C:\\Windows\\Web\\Wallpaper\\Windows\\life path.jpg");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.connect();
+            InputStream in = urlConnection.getInputStream();
+            String filePath = url.getFile();
+            int pos = filePath.lastIndexOf("/");
+            String fileName = filePath.substring(pos + 1);
+            FileOutputStream out = new FileOutputStream("D:/" + fileName);
+            byte[] bytes = new byte[urlConnection.getContentLength()];
+            int len = in.read(bytes);
+            while (len != -1) {
+                out.write(bytes, 0, len);
+                in.read(bytes);
+            }
+            out.close();
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
